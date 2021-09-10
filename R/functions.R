7ea22476931dce474ade16cc5fc7ea6f4014d7f3
Bruno Montezano <brunodoyt@gmail.com>
2021-09-10 10:36:09 -0300

    first real version

--- R/functions.R
+++ R/functions.R
@@ -1,14 +1,11 @@
 library(magrittr, include.only = "%>%")
 
-#' Gerar tabela com países e respectivas capitais
+#' Generate table with countries, capitals e continents
 #'
-#' @param caminho Caminho onde será salva a base de dados no seu computador.
-#'
-#' @return Retorna uma tibble com a tabela com os países, capitais e continentes.
+#' @return Tibble containing countries, capitals and their respective continents.
 #' @export
 #'
-#' @examples
-gerar_tabela <- function(caminho) {
+gerar_tabela <- function() {
 
 paises_america <- httr::GET("https://brasilescola.uol.com.br/geografia/paises-america.htm")
 paises_africa <- httr::GET("https://brasilescola.uol.com.br/geografia/paises-da-africa.htm")
@@ -31,7 +28,7 @@ africa <- paises_africa %>%
   dplyr::mutate(capital = stringr::str_extract(pais, "\\(.*?\\)"),
     capital = stringr::str_remove_all(capital, "\\(|\\)"),
     pais = stringr::str_squish(stringr::str_remove_all(pais, "\\(.*.")),
-    continente = "África")
+    continente = "\u00c1frica")
 
 europa <- paises_europa %>%
   httr::content() %>%
@@ -49,7 +46,7 @@ oceania <- paises_oceania %>%
   dplyr::slice(-1, -7, -15, -23, -34) %>%
   purrr::set_names("pais", "capital") %>%
   dplyr::mutate(pais = stringr::str_squish(stringr::str_remove_all(pais, "\\(.*.")),
-    capital = ifelse(capital == "não possui capital", NA, capital),
+    capital = ifelse(capital == "n\u00e3o possui capital", NA, capital),
     continente = "Oceania")
 
 asia <- paises_asia %>%
@@ -59,7 +56,7 @@ asia <- paises_asia %>%
   purrr::set_names("pais", "capital", "dropar") %>%
   dplyr::select(-dropar) %>%
   dplyr::filter(!stringr::str_detect(pais, "google")) %>%
-  dplyr::mutate(continente = "Ásia")
+  dplyr::mutate(continente = "\u00c1sia")
 
 continentes <- dplyr::bind_rows(america, africa, asia, europa, oceania)
 
---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c(
+  "capital", "dropar", "pais"
+))
